7cfec25f6294590f5969c85be6f5602fe33f1b11
MOHAMED MUBARISH.M.Y <mohamedmubarish30@gmail.com>
2026-08-22 20:45:35 +0530

    Prime Number

---
+++ prime.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+class prime {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a number:");
+        int n = sc.nextInt();
+
+        boolean prime = true;
+
+        if (n <= 1)
+            prime = false;
+        else {
+            for (int i = 2; i < n; i++) {
+                if (n % i == 0) {
+                    prime = false;
+                    break;
+                }
+            }
+        }
+
+        if (prime)
+            System.out.println(n + " is a Prime Number");
+        else
+            System.out.println(n + " is not a Prime Number");  
+        sc.close();
+    }
+}
