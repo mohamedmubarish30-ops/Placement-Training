08422c514fd5a3afbee97c59290b9446d0ca49c8
MOHAMED MUBARISH.M.Y <mohamedmubarish30@gmail.com>
2026-08-22 22:02:57 +0530

    Count Number Digits

---
+++ Countdigits.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class Countdigits {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a number:");
+        int n = sc.nextInt();
+
+        int count = 0;
+
+        while (n != 0) {
+            count++;
+            n = n / 10;
+        }
+
+        System.out.println("Number of digits: " + count);
+        sc.close();
+    }
+}
